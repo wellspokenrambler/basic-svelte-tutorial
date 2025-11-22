9c0c8ed65d7d75299b677a055e1f2b20d14f75bb
wellspokenrambler <36102623+wellspokenrambler@users.noreply.github.com>
2025-11-22 18:19:41 +0000

    formatted headings, calls to action, and lists site-wide, turned unordered list on main page to ordered list and removed redundant number references

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,13 @@
 
 <h1>Isabelle May - Basic Svelte Tutorial Work Examples:</h1>
-<ul>
-	<li><a href="/1_introduction">1 - Introduction</a></li>
-	<li><a href="/2_reactivity">2 - Reactivity</a></li>
-	<li><a href="/3_props">3 - Props</a></li>
-	<li><a href="/4_logic">4 - Logic</a></li>
-	<li><a href="/5_events">5 - Events</a></li>
-	<li><a href="/6_bindings">6 - Bindings</a></li>
-	<li><a href="/7_classes_styles">7 - Classes & Styles</a></li>
-	<li><a href="/8_actions">8 - Actions</a></li>
-	<li><a href="/9_transitions">9 - Transitions</a></li>
-</ul>
+<ol>
+	<li><a href="/1_introduction">Introduction</a></li>
+	<li><a href="/2_reactivity">Reactivity</a></li>
+	<li><a href="/3_props">Props</a></li>
+	<li><a href="/4_logic">Logic</a></li>
+	<li><a href="/5_events">Events</a></li>
+	<li><a href="/6_bindings">Bindings</a></li>
+	<li><a href="/7_classes_styles">Classes & Styles</a></li>
+	<li><a href="/8_actions">Actions</a></li>
+	<li><a href="/9_transitions">Transitions</a></li>
+</ol>
